f0b9f3f463a84d0a10552736e885df1613d87715
Satyajit Pratihar <satyajitpratihar96@gmail.com>
2025-11-15 15:52:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,6 +81,37 @@ showMoreBtn.addEventListener("click", () => {
     searchImages();
 });
 
-searchBox.addEventListener('focus', () => {
-    searchBox.setAttribute('autocomplete', 'new-password');
-});
+;
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let stars = [];
+
+for(let i = 0; i < 200; i++) {
+    stars.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        size: Math.random() * 2,
+        speed: Math.random() * 0.5
+    });
+}
+
+function animateStars() {
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillStyle = "#00faff";
+    stars.forEach(star => {
+        ctx.beginPath();
+        ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+        ctx.fill();
+        star.y -= star.speed;
+        if(star.y < 0) star.y = canvas.height;
+    });
+
+    requestAnimationFrame(animateStars);
+}
+
+animateStars();
